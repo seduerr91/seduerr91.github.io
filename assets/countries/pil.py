3e3afceb531cdf7b4ef47af91f82cbb7f398f078
Sebastian Dürr <duerr.sebastian@gmail.com>
2022-08-19 21:05:51 -0700

    adapted pictures

--- assets/countries/pil.py
+++
@@ -1,9 +0,0 @@
-import os
-from PIL import Image
-countries = []
-for file in os.listdir('resized'):
-    countries.append(file.partition(".")[0])
-    # print(f'![image](../assets/countries/resized/{file})\n')
-
-print(sorted(countries))
-print(len(countries))
