788e23e642c2ca587ee62c92160dec688847903a
Sebastian Dürr <duerr.sebastian@gmail.com>
2022-08-19 18:05:34 -0700

    test

--- assets/countries/pil.py
+++ assets/countries/pil.py
@@ -1,11 +1,6 @@
 import os
 from PIL import Image
 
-for file in os.listdir('original_images'):
-    f_img = 'original_images/'+file
-    if f_img.endswith('.jpg' or '.JPG' or '.HEIC'):
-        try:
-            img = Image.open(f_img)
-            img = img.resize((1024, 768))
-            img.save('resized/' + file)
-        except: pass
+for file in os.listdir('resized'):
+    print(f'### {file.partition(".")[0]}')
+    print(f'![image](../assets/countries/resized/{file})\n')
